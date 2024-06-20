e6a648950c9fbfc49efa5922e7e599e30b2c1cb5
tenzyu <tenzyu.on@gmail.com>
2024-06-21 02:02:16 +0900

    feat: server creation api

--- src/components/modals/initial-modal.tsx
+++ src/components/modals/initial-modal.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 import {
     Dialog,
@@ -36,6 +38,7 @@ const formSchema = z.object({
 })
 
 const InitialModal = () => {
+    const router = useRouter()
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -43,10 +46,17 @@ const InitialModal = () => {
             imageUrl: ''
         }
     })
-
     const isLoading = form.formState.isSubmitting
+
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post('/api/servers', values)
+            form.reset()
+            router.refresh()
+            window.location.reload()
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     // https://nextjs.org/docs/messages/react-hydration-error {{{
---
+++ src/lib/current-profile.ts
@@ -0,0 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
+
+import { prisma } from "./db";
+
+export const currentProfile = async () => {
+    const { userId } = auth()
+
+    if (!userId) return null
+
+    const profile = await prisma.profile.findUnique({
+        where: { userId }
+    })
+
+    return profile
+}
