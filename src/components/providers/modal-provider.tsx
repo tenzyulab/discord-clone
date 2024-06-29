41deefc146473bf6f3face6cc70cdb0d28344fdd
tenzyu <tenzyu.on@gmail.com>
2024-06-30 08:09:42 +0900

    feat: invite modal

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { CreateServerModal } from '@/components/modals/create-server-modal'
 import { useEffect, useState } from 'react'
+
+import { CreateServerModal } from '@/components/modals/create-server-modal'
+import { InviteModal } from '@/components/modals/invite-modal'
+
 
 export const ModalProvider = () => {
     // https://nextjs.org/docs/messages/react-hydration-error {{{
@@ -13,6 +16,7 @@ export const ModalProvider = () => {
     return (
         <>
             <CreateServerModal />
+            <InviteModal />
         </>
     )
 }
